630b93889ba303e81613c04ee56e2ecf7dea80f0
Sulthan Allaudeen <sulthanallaudeen@users.noreply.github.com>
2015-08-12 14:00:05 +0530

    Update app.php

--- includes/app.php
+++ includes/app.php
@@ -1,6 +1,6 @@
 <?php
 #$url = 'http://localhost/app/GoogleMapsApi/';
 #$url = 'http://sysaxiom.com/app/GoogleMapsApi/';
-$url = 'https://sys-googlemapsapi.herokuapp.com';
+$url = 'https://sys-googlemapsapi.herokuapp.com/';
 $StartTime = microtime(true); #Starting the Timer
 ?>
