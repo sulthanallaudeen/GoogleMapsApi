c823e82495ed56a6959dad77d96cf5ed9471ef19
Sulthan Allaudeen <sa@sulthanallaudeen.com>
2017-01-16 09:53:37 +0530

    Assets updated

--- includes/app.php
+++ includes/app.php
@@ -1,6 +1,6 @@
 <?php
 $url = 'http://localhost/workspace/GoogleMapsApi/';
 #$url = 'http://sysaxiom.com/app/GoogleMapsApi/';
-$url = 'https://sa-googlemaps-api/';
+$url = 'https://sa-googlemaps-api.herokuapp.com/';
 $StartTime = microtime(true); #Starting the Timer
 ?>
